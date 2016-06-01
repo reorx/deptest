5e7072801e305f4ca5cabc1e846d113ba7a43e1e
reorx <novoreorx@gmail.com>
2016-06-01 20:31:49 +0800

    capture stdout

--- deptest.py
+++ deptest.py
@@ -6,12 +6,16 @@ import re
 import sys
 import imp
 import logging
+import argparse
 import traceback
+from StringIO import StringIO
 # import functools
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 lg = logging.getLogger('deptest')
+
+LINE_WIDTH = 70
 
 
 def gprint(s):
@@ -38,6 +42,7 @@ def load_test_file(filepath):
 def run_test_file(module):
     runner = ModuleRunner(module)
     runner.dispatch()
+    return runner
 
 
 class ModuleRunner(object):
@@ -46,7 +51,7 @@ class ModuleRunner(object):
     module_teardown_pattern = re.compile(r'^global_teardown$')
 
     def __init__(self, module):
-        lg.debug('ModuleRunner init')
+        lg.debug('ModuleRunner init: %s', module)
         entries = []
         entries_dict = {}
         module_setup = None
@@ -91,11 +96,12 @@ class ModuleRunner(object):
         lg.debug('entries_dict: %s', self.entries_dict)
         for entry in self.entries:
             deps = traverse_entry_dependencies(entry, self.entries_dict)
-            print 'entry {} depend on {}'.format(entry.__name__, deps)
+            lg.debug('entry {} depend on {}'.format(entry.__name__, deps))
 
     def dispatch(self):
         lg.debug('ModuleRunner dispatch')
         states = defaultdict(get_state)
+        self.states = states
         self._dispatch(self.entries, states)
 
     def _dispatch(self, entries, states):
@@ -126,39 +132,112 @@ class ModuleRunner(object):
     def run_entry(self, entry, states):
         lg.debug('run entry %s', entry)
         state = states[entry]
+        entry_runner = EntryRunner(entry, state, self)
+        entry_runner.run()
+
+    def __str__(self):
+        return '<ModuleRunner: {}>'.format(self.module.__name__)
+
+
+class EntryRunner(object):
+    def __init__(self, entry, state, module_runner):
+        self.entry = entry
+        self.state = state
+        self.module_runner = module_runner
+        self.stdout = []
+        self._buf = None
+        self.output = None
+
+    def run(self):
+        entry = self.entry
+        state = self.state
+
+        # capture_stdout
+        self.capture_stdout()
+
         try:
-            # get arguments
+            # TODO get arguments
             entry()
-        except Exception as e:
-            str(e)
-            # traceback
-            traceback.print_exc()
+        except:
+            state['traceback'] = traceback.format_exc()
             state['passed'] = False
         else:
             state['passed'] = True
         finally:
             state['executed'] = True
 
-        self.log_entry_state(entry, state)
+        # get output
+        state['captured_stdout'] = self._get_buffer()
 
-    def log_entry_state(self, entry, state):
+        # restore_stdout
+        self.restore_stdout()
+
         # log state
-        if state['unmet']:
-            status = 'UNMET'
-        elif state['passed']:
-            status = 'PASSED'
+        self.log_state()
+
+    def capture_stdout(self):
+        self.stdout.append(sys.stdout)
+        self._buf = StringIO()
+        # Python 3's StringIO objects don't support setting encoding or errors
+        # directly and they're already set to None.  So if the attributes
+        # already exist, skip adding them.
+        if (not hasattr(self._buf, 'encoding') and
+                hasattr(sys.stdout, 'encoding')):
+            self._buf.encoding = sys.stdout.encoding
+        if (not hasattr(self._buf, 'errors') and
+                hasattr(sys.stdout, 'errors')):
+            self._buf.errors = sys.stdout.errors
+        sys.stdout = self._buf
+
+    def restore_stdout(self):
+        while self.stdout:
+            sys.stdout = self.stdout.pop()
+        lg.debug('restored %s', sys.stdout)
+
+    def _get_buffer(self):
+        if self._buf is not None:
+            return self._buf.getvalue()
+
+    def log_state(self):
+        entry = self.entry
+        state = self.state
+        status = get_state_status(state)
+        full_name = '{}.{}'.format(self.module_runner.module.__name__, entry.__name__)
+
+        if status == 'FAILED':
+            print '=' * LINE_WIDTH
+            print '{}... {}'.format(full_name, status)
+            print '-' * LINE_WIDTH
+            print state['traceback']
+            print '-------------------- >> begin captured stdout << ---------------------'
+            print state['captured_stdout']
+            print '--------------------- >> end captured stdout << ----------------------'
+            print ''
+            print '-' * LINE_WIDTH
         else:
-            status = 'FAILED'
-        print '{}.{}... {}'.format(self.module.__name__, entry.__name__, status)
+            print '{}... {}'.format(full_name, status)
 
 
 def get_state():
     return {
+        'unmet': False,
         'executed': False,
         'passed': False,
-        'unmet': False,
         'return_value': None,
+        'traceback': None,
+        'captured_stdout': None,
+        'captured_logging': None,
     }
+
+
+def get_state_status(state):
+    if state['unmet']:
+        status = 'UNMET'
+    elif state['passed']:
+        status = 'PASSED'
+    else:
+        status = 'FAILED'
+    return status
 
 
 def set_state(state, key, value):
@@ -242,11 +321,80 @@ def with_setup(setup_func=None, teardown_func=None):
     pass
 
 
+_ignore_dirs = ['.git']
+
+
+_test_file_pattern = re.compile(r'^(.+_test|test_.+)\.py$')
+
+
+def walk_dir(dirpath, filepaths):
+    for root, dirs, files in os.walk(dirpath):
+        for name in files:
+            if _test_file_pattern.match(name):
+                filepath = os.path.join(root, name)
+                filepaths.append(filepath)
+
+        for dir in dirs:
+            if dir in _ignore_dirs:
+                lg.debug('ignore %s %s', root, dir)
+                dirs.remove(dir)
+
+
+def log_summary(runners):
+    summary = Counter({'UNMET': 0, 'PASSED': 0, 'FAILED': 0, 'total': 0})
+    for runner in runners:
+        lg.debug('runner %s', runner)
+        if not runner.entries:
+            continue
+        for entry, state in runner.states.iteritems():
+            status = get_state_status(state)
+            summary[status] += 1
+            summary['total'] += 1
+
+    print '\n' + '-' * LINE_WIDTH
+    print 'Ran {total} tests, PASSED {PASSED}, FAILED {FAILED}, UNMET {UNMET}'.format(**summary)
+
+
+def main():
+    # the `formatter_class` can make description & epilog show multiline
+    parser = argparse.ArgumentParser(description="", epilog="", formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # arguments
+    parser.add_argument('paths', metavar="PATH", type=str, help="files or dirs to scan", nargs='+')
+
+    # options
+    parser.add_argument('-a', '--aa', type=int, default=0, help="")
+    parser.add_argument('-b', '--bb', type=str, help="")
+    parser.add_argument('-s', '--nocapture', action='store_true', help="Don't capture stdout (any stdout output will be printed immediately)")
+    parser.add_argument('--debug', action='store_true', help="Set logging level to debug for deptest logger")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    logging.basicConfig(
+        level=logging_level,
+        format='[%(levelname)s %(module)s:%(lineno)d] %(message)s')
+
+    filepaths = []
+
+    for path in args.paths:
+        if os.path.isdir(path):
+            walk_dir(path, filepaths)
+        else:
+            filepaths.append(path)
+
+    runners = []
+    for filepath in filepaths:
+        module = load_test_file(filepath)
+        runner = run_test_file(module)
+        runners.append(runner)
+
+    log_summary(runners)
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-
-    filename = sys.argv[1]
-
-    module = load_test_file(filename)
-
-    run_test_file(module)
+    main()
---
+++ test/another_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def test_a():
+    print 'wtf'
+    assert 0
